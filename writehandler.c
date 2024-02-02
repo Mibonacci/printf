50676146205c77c8e0bda9b5757d7ebb538728c2
Mibonacci <mohammadramdaoui@gmail.com>
2024-02-02 23:41:27 +0100

    done

--- writehandler.c
+++ writehandler.c
@@ -112,7 +112,6 @@ int unsgndhandl(int is_negative, int ind,
 	char buffer[],
 	int flags, int width, int precision, int size)
 {
-	/* The number is stored at the bufer's right and starts at position i */
 	int length = BUFF_SIZE - ind - 1, i = 0;
 	char padd = ' ';
 
@@ -120,7 +119,7 @@ int unsgndhandl(int is_negative, int ind,
 	UNUSED(size);
 
 	if (precision == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
-		return (0); /* printf(".0d", 0)  no char is printed */
+		return (0);
 
 	if (precision > 0 && precision < length)
 		padd = ' ';
